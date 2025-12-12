fbdd0140cc8083e5105247534dea9974d5862338
unknown <orifepaul30@gmail.com>
2025-12-12 17:06:25 +0100

    updated website logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CELZ5 Citation</title>
+    <!-- Favicon -->
+<link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/feather-icons"></script>
